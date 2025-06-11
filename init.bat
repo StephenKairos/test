e3fde9677fd964f2fc7ff22e6270f1fcacd835a2
StephenKairos <stephenkairos.reyes@sjsu.edu>
2025-06-10 17:04:01 -0700

    Committing Dummy File: Tue 06/10/2025 17:04:01.45

--- init.bat
+++ init.bat
@@ -13,13 +13,13 @@ echo Committing Dummy File: %DATE% %TIME% > datetime.txt
 echo Forcing Login Sequence...
 git commit -F datetime.txt
 del datetime.txt
-git pull
-git push
+git pull origin master
+git push origin master
 del dummy.txt
 echo Deleting Dummy file...
 git add .
 echo Deleting Dummy file: %DATE% %TIME% > datetime.txt
 git commit -F datetime.txt
 del datetime.txt
-git pull
-git push
+git pull origin master
+git push origin master
